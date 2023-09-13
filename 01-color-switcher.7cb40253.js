9b9dd02eb9f56a7c035a2a69d101eb04f51aec10
Reiterovich <130479193+Reiterovich@users.noreply.github.com>
2023-09-13 19:48:20 +0000

    Deploying to gh-pages from @ Reiterovich/goit-js-hw-09@eaa11807ffb1169fde25f5b4e5335d9e95c7d598 🚀

---
+++ 01-color-switcher.7cb40253.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");console.log(t),console.log(e);let d=null;t.addEventListener("click",(function(o){d=setInterval(l,1e3),t.disabled=!0,e.disabled&&(t.disabled=!0,e.disabled=!1)}));const l=()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;o.style.backgroundColor=t};e.addEventListener("click",(function(o){t.disabled&&(t.disabled=!1,e.disabled=!0,clearInterval(d))}));
+//# sourceMappingURL=01-color-switcher.7cb40253.js.map
